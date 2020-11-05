a9d61d8feffa6c15ade9d5306adb1ed9a6f1daaf
Miguel Concepcion <miguel.concepcion@outlook.com>
2020-11-05 23:59:11 +0800

    add login url

--- spring_dream/api/v1/urls.py
+++ spring_dream/api/v1/urls.py
@@ -1,8 +1,14 @@
+from django.urls import path
+
 from rest_framework.routers import DefaultRouter
-from api.v1.views import UserViewSet
+
+from api.v1.views.auth import CustomOAuth2Token
+from api.v1.views.users import UserViewSet
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
 
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('api-token-auth/', CustomOAuth2Token.as_view()),
+] + router.urls
