13c408796fbac4f6e00d3b1bd1bcaed4c66ee12b
Miguel Concepcion <miguel.concepcion@outlook.com>
2020-11-05 23:07:21 +0800

    use standard REST methods than actions

--- spring_dream/api/v1/views.py
+++ spring_dream/api/v1/views.py
@@ -1,132 +1,83 @@
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
-from django.core.validators import ValidationError
 from django.db import transaction
 
-from rest_framework import viewsets, status, permissions
+from rest_framework import status
+from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from api.v1.serializers import (
-    UserSerializer,
-    UserIncompleteSerializer,
-    UserLoginSerializer,
-    UserCreateSerializer,
-)
-from core.utils import send_activation_mail
-from core.utils import create_application, create_token
 from oauth2_provider.models import AccessToken
+
+from api.v1.serializers import UserCreateSerializer
+from api.v1.serializers import UserLoginSerializer
+from api.v1.serializers import UserPartialSerializer
+from api.v1.serializers import UserSerializer
+from core.permissions import IsOwnerOrReadOnly
+from core.utils import create_application
+from core.utils import create_token
+from core.utils import send_activation_mail
 
 
 class UserViewSet(viewsets.ModelViewSet):
-    '''
+    """
     User ViewSet
     Contains all the operations to the model User
-    '''
+    """
 
     model = User
     serializer_class = UserSerializer
     queryset = User.objects.all()
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [IsOwnerOrReadOnly, ]
 
-    def list(self, request):
-        '''
-        Resource:
-        api/v1/users/
-        Only authenticated users can see email field
-        as well as last name field.
-        '''
-        if request.auth:
-            serializer = self.get_serializer(self.queryset, many=True)
+    def get_serializer_class(self):
+        """
+        Returns a different serializer depending
+        if the user is authenticated or not
+        """
+        if self.request.auth and self.request.user.is_active:
+            serializer = self.serializer_class
         else:
-            serializer = UserIncompleteSerializer(self.queryset, many=True)
+            serializer = UserPartialSerializer
 
-        return Response(serializer.data)
+        return serializer
 
-    @action(
-        detail=False,
-        methods=['patch'],
-        permission_classes=[permissions.IsAuthenticated]
-    )
-    def change_password(self, request):
-        '''
-        Resource:
-        api/v1/users/change_password/
-        '''
-        serializer = self.get_serializer(data=request.data)
-        if serializer.is_valid():
-            user = request.user
-            user.set_password(request.data['password'])
-            user.save()
-            return Response(
-                {'message': 'password changed.'},
-                status.HTTP_200_OK
-            )
-        else:
-            return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
-
-    @action(detail=False, methods=['post'])
-    def signup(self, request):
-        '''
-        Resource:
-        api/v1/users/signup/
-        Creates inactive user instance and
-        sends an activation email to the user.
-        '''
-
+    @transaction.atomic
+    def create(self, request, *args, **kwargs):
         serializer = UserCreateSerializer(data=request.data)
-
-        if serializer.is_valid():
-            user = User(
-                first_name=serializer.data.get('first_name') or '',
-                last_name=serializer.data.get('last_name') or '',
-                email=serializer.data['email'],
-                username=serializer.data['email'],
-                is_active=False
-            )
-
-            user.set_password(request.data['password'])
-            try:
-                user.full_clean()
-            except ValidationError as e:
-                return Response(
-                    {'error': 'cause:{}'.format(e)},
-                    status=status.HTTP_400_BAD_REQUEST
-                )
-            else:
-                user.save()
-                app = create_application(user)
-                create_token(user, app)
-                # TO DO: convert mail sending to a celery task
-                send_activation_mail(user=user)
-                return Response(
-                    serializer.data,
-                    status=status.HTTP_201_CREATED
-                )
-        else:
-            return Response(
-                serializer.errors,
-                status=status.HTTP_400_BAD_REQUEST
-            )
-
-    @action(detail=False, methods=['get'])
-    def activate(self, request, pk=None):
-        '''
-        Resource:
-        api/v1/users/<pk>/activate/
-        Sets the user's is_active = True after validation of the token
-        '''
-        if not request.auth:
-            return Response(
-                {'message': 'invalid token'},
-                status=status.HTTP_401_UNAUTHORIZED
-            )
-        user = request.user
-        user.is_active = True
-        user.save()
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
         return Response(
-            {'message': 'account activated'},
-            status=status.HTTP_200_OK
+            serializer.data,
+            status=status.HTTP_201_CREATED,
+            headers=headers
         )
+
+    def perform_create(self, serializer):
+        # Populate username field with email
+        serializer.validated_data['username'] = serializer.validated_data[
+            'email']
+        serializer.validated_data['is_active'] = False
+        serializer.save()
+        # Create app and access token for the user
+        user = serializer.instance
+        user.set_password(serializer.initial_data['password'])
+        app = create_application(user)
+        create_token(user, app)
+        transaction.on_commit(lambda: send_activation_mail(user=user))
+
+    @transaction.atomic
+    def update(self, request, *args, **kwargs):
+        return super().update(request, *args, **kwargs)
+
+    def perform_update(self, serializer):
+        """
+        Support password change on PATCH
+        """
+        password = serializer.initial_data.get('password')
+        if password:
+            serializer.instance.set_password(password)
+        return super().perform_create(serializer)
 
     @action(detail=False, methods=['post'])
     def login(self, request):
